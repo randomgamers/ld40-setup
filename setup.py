10844a422dbe257e4c855549428bbdcc01f9c95e
Andrej Hoos <andrej.hoos@gmail.com>
2017-12-01 23:52:04 +0100

    :lipstick: setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,25 @@
 from cx_Freeze import setup, Executable
+import sys
 
 # Dependencies are automatically detected, but it might need
 # fine tuning.
-buildOptions = dict(packages=['ld40_setup', 'ld40_setup.resources', 'ld40_setup.resources.images', 'ld40_setup.resources.sounds'],
-                    excludes = [],
-                    include_files=[('ld40_setup/resources', 'ld40_setup/resources')]
-                    )
 
-import sys
-base = 'Win32GUI' if sys.platform=='win32' else None
+buildOptions = dict(
+    packages=['ld40_setup'],
+    excludes=[],
+    include_files=[('ld40_setup/resources', 'ld40_setup/resources')]
+)
+
+base = 'Win32GUI' if sys.platform == 'win32' else None
 
 executables = [
     Executable('sample.py', base=base)
 ]
 
-setup(name='',
-      version = '1.0',
-      description = '',
-      options = dict(build_exe = buildOptions),
-      executables = executables)
+setup(
+    name='Sample',
+    version='1.0',
+    description='Sample',
+    options=dict(build_exe=buildOptions),
+    executables=executables
+)
