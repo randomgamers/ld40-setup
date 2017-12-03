bdf7958e6d13e4409476e99fdc53fa66804beaf1
Petr Belohlavek <me@petrbel.cz>
2017-12-03 12:29:06 +0100

    texts

--- ld40_setup/level.py
+++ ld40_setup/level.py
@@ -2,55 +2,68 @@ import os
 import numpy as np
 from typing import List, Type
 
+import pygame
+
 from . import config
 from .sprites import Player, hostages, Guard, Camera, Wall, Floor, Door
+from .utils import game_pixel_to_coord
 
 
 class Level:
     def __init__(self):
         assert hasattr(self, 'level_num')
 
+        # load level
         level_file = os.path.join(config.PROJECT_ROOT, config.RESOURCES_ROOT, config.LEVELS_DIR,
                                   'level{}.map'.format(self.level_num))
 
         with open(level_file, 'r') as fin:
             self.map = [list(line)[:-1] for line in fin]
 
+        # build walls
         self.wall_coords = [(col_num, row_num) for row_num, row in enumerate(self.map)
                                                for col_num, tile in enumerate(row)
                                                if tile == 'W']
-        self.walls = []
-        for x, y in self.wall_coords:
-            wall = Wall(x, y)
-            self.walls.append(wall)
+        self.walls = [Wall(x, y) for x, y in self.wall_coords]
 
+        # select entry coord
         self.entry_coords = [(col_num, row_num) for row_num, row in enumerate(self.map)
                                                 for col_num, tile in enumerate(row)
                                                 if tile == 'E']
         assert len(self.entry_coords) == 1, 'Too many/few level entries'
         self.entry_coord = self.entry_coords[0]
 
+        # build doors
         self.doors_coords = [(col_num, row_num) for row_num, row in enumerate(self.map)
                                                 for col_num, tile in enumerate(row)
                                                 if tile == 'D' or tile == 'F' or tile == 'G']
-        self.doors = []
-        for x, y in self.doors_coords:
-            door_tile = Door(x, y, self.map[y][x])
-            self.doors.append(door_tile)
+        self.doors = [Door(x, y, self.map[y][x]) for x, y in self.doors_coords]
 
+        # build floor
         self.floor_coords = [(col_num, row_num) for row_num, row in enumerate(self.map)
                              for col_num, tile in enumerate(row)
                              if tile == '.']
+        self.floor = [Floor(x,y) for x, y in self.floor_coords]
 
-        self.floor = []
-        for x, y in self.floor_coords:
-            floor_tile = Floor(x, y)
-            self.floor.append(floor_tile)
-
+        # player
         self.player = Player(position=self.entry_coord, walls=self.walls)
+
+        # sprites
         self.guards = []
         self.cameras = []
         self.hostages = []
+        self.texts = []
+
+        # text config
+        self.font = pygame.font.Font(None, config.INGAME_TEXT_SIZE)
+
+    def add_text(self, message, tile_x, tile_y):
+        label = self.font.render(message, 1, config.INGAME_TEXT_COLOR)
+        width = label.get_rect().width
+        height = label.get_rect().height
+        position_x, position_y = game_pixel_to_coord((tile_x, tile_y))
+        self.texts.append([message, label, (width, height), (position_x, position_y)])
+
 
     @property
     def map_shape(self):
@@ -94,6 +107,10 @@ class Level1(Level):
             hostages.RegularGuy(position=(26, 25), player=self.player, entry_tile=self.entry_coord),
             hostages.RegularGuy(position=(58, 23), player=self.player, entry_tile=self.entry_coord),
             hostages.RegularGuy(position=(19, 10), player=self.player, entry_tile=self.entry_coord),
+        ]
+
+        self.texts = [
+
         ]
 
 
