59c67dd83d730bd178810989f3609c078a42f404
Petr Belohlavek <me@petrbel.cz>
2017-12-03 09:57:37 +0100

    menu colors

--- ld40_setup/menu.py
+++ ld40_setup/menu.py
@@ -5,45 +5,50 @@ from ld40_setup import config
 
 class Menu:
 
-    def __init__(self, screen, items, bg_color, font_size, message, font_color=(255, 255, 255), font=None):
+    def __init__(self, screen, menu_items, background_color,
+                 message, message_size=config.MESSAGE_SIZE, message_color=config.MESSAGE_COLOR,
+                 title_size=config.TITLE_SIZE, title_color=config.TITLE_COLOR,
+                 item_size=config.ITEM_SIZE, item_color=config.ITEM_COLOR,
+                 font=None):
+
         # save stuff
         self.screen = screen
-        self.scr_width = self.screen.get_rect().width
-        self.scr_height = self.screen.get_rect().height
+        self.screen_width = self.screen.get_rect().width
+        self.screen_height = self.screen.get_rect().height
 
-        self.bg_color = bg_color
+        self.background_color = background_color
         self.clock = pygame.time.Clock()
-
-        self.font = pygame.font.Font(None, 48)
-        self.font_color = font_color
 
         self.items = []
 
         # title
-        title_label = self.font.render(config.GAME_NAME, 1, font_color)
+        title_font = font or pygame.font.Font(None, title_size)
+        title_label = title_font.render(config.GAME_NAME, 1, title_color)
         label_width = title_label.get_rect().width
         label_height = title_label.get_rect().height
-        label_position_x = (self.scr_width / 2) - (label_width / 2)
-        label_position_y = self.scr_height / 4
+        label_position_x = (self.screen_width / 2) - (label_width / 2)
+        label_position_y = self.screen_height / 4
         self.items.append([config.GAME_NAME, title_label, (label_width, label_height), (label_position_x, label_position_y)])
 
         # message
-        title_label = self.font.render(message, 1, font_color)
-        label_width = title_label.get_rect().width
-        label_height = title_label.get_rect().height
-        label_position_x = (self.scr_width / 2) - (label_width / 2)
-        label_position_y = 3 * self.scr_height / 8
-        self.items.append([message, title_label, (label_width, label_height), (label_position_x, label_position_y)])
+        message_font = font or pygame.font.Font(None, message_size)
+        message_label = message_font.render(message, 1, message_color)
+        label_width = message_label.get_rect().width
+        label_height = message_label.get_rect().height
+        label_position_x = (self.screen_width / 2) - (label_width / 2)
+        label_position_y = 3 * self.screen_height / 8
+        self.items.append([message, message_label, (label_width, label_height), (label_position_x, label_position_y)])
 
-        # generate menu items
-        for index, item in enumerate(items):
-            label = self.font.render(item, 1, font_color)
+        # menu items
+        item_font = font or pygame.font.Font(None, item_size)
+        for index, item in enumerate(menu_items):
+            label = item_font.render(item, 1, item_color)
             label_width = label.get_rect().width
             label_height = label.get_rect().height
 
-            label_position_x = (self.scr_width / 2) - (label_width / 2)
-            text_block_height = len(items) * label_height
-            label_position_y = (self.scr_height / 2) - (text_block_height / 2) + (index * label_height)
+            label_position_x = (self.screen_width / 2) - (label_width / 2)
+            text_block_height = len(menu_items) * label_height
+            label_position_y = (self.screen_height / 2) - (text_block_height / 2) + (index * label_height)
 
             self.items.append([item, label, (label_width, label_height), (label_position_x, label_position_y)])
 
@@ -64,7 +69,7 @@ class Menu:
                 result = self.event_handler(event)
 
             # render the background
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.background_color)
 
             # render menu items
             for name, label, (width, height), (posx, posy) in self.items:
@@ -77,14 +82,15 @@ class Menu:
 class MainMenu(Menu):
     def __init__(self, screen):
         items = ['(S) Start', '(Q) Quit']
-        super().__init__(screen, items=items, bg_color=(0,0,0), font_size=30, message='Main Menu')
+        super().__init__(screen, menu_items=items, background_color=(40, 42, 56),
+                         message='Main Menu')
 
 
 class SuccessMenu(Menu):
 
     def __init__(self, screen, num_level):
         items = ['(S) Next Level', '(Q) Main Menu']
-        super().__init__(screen, items=items, bg_color=(0,0,0), font_size=30,
+        super().__init__(screen, menu_items=items, background_color=(44,51,38),
                          message='Level #{} Complete'.format(num_level))
 
 
@@ -92,7 +98,7 @@ class FailureMenu(Menu):
 
     def __init__(self, screen, num_level):
         items = ['(S) Try Again', '(Q) Main Menu']
-        super().__init__(screen, items=items, bg_color=(0,0,0), font_size=30,
+        super().__init__(screen, menu_items=items, background_color=(59,37,37),
                          message = 'Level #{} Failed'.format(num_level))
 
 
@@ -100,4 +106,5 @@ class GameWonMenu(Menu):
 
     def __init__(self, screen):
         items = ['(S) Play Whole Game Again', '(Q) Main Menu']
-        super().__init__(screen, items=items, bg_color=(0,0,0), font_size=30, message='Game Completed')
+        super().__init__(screen, menu_items=items, background_color=(128,97,37), title_color=(0,0,0),
+                         message='Game Completed')
